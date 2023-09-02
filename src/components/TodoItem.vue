18c5d7d0618106881a299884f8ee8cf79465008c
Hoceine <hoceineelidrissi@gmail.com>
2023-09-02 22:57:17 +0100

    Add the vue draggable to the app

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -21,58 +21,60 @@ defineEmits([
 </script>
 
 <template>
-  <li
-    class="flex w-[30rem] items-center relative rounded-md p-4 bg-gray-200 gap-5 hover:shadow-lg group hover:shadow-gray-300 mb-4 mx-10 transition-all duration-500 hover:scale-[1.02]"
-  >
-    <input
-      type="checkbox"
-      @input="$emit('toggle-complete', index)"
-      :checked="todo.isComplete"
-      class="rounded-full appearance-none w-6 h-6 shadow-md shadow-gray-600 checked:bg-purple-500 bg-slate-100"
-    />
-    <div class="">
-      <input
-        type="text"
-        v-if="todo.isEditing"
-        @input="$emit('update-todo', $event.target.value, index)"
-        class="rounded-lg px-3 object-fill text-gray-600"
-        :value="todo.todo"
-      />
-      <span
-        v-else
-        class="text-gray-700 pe-9"
-        :class="{ 'line-through': todo.isComplete }"
-        >{{ todo.todo }}</span
-      >
-    </div>
+  <li>
     <div
-      class="flex gap-3 absolute right-3 group-hover:opacity-100 opacity-0 transition duration-500"
+      class="drag-div flex w-[30rem] items-center relative rounded-md p-4 bg-gray-200 gap-5 hover:shadow-lg group hover:shadow-gray-300 mb-4 mx-10 transition-all duration-500 hover:scale-[1.02]"
     >
-      <Icon
-        icon="ph:pen-fill"
-        @click="$emit('edit-todo', index)"
-        class="cursor-pointer"
-        :key="todo.id"
-        v-if="!todo.isEditing"
-        color="#34D399"
-        width="25"
+      <input
+        type="checkbox"
+        @input="$emit('toggle-complete', index)"
+        :checked="todo.isComplete"
+        class="rounded-full appearance-none w-6 h-6 shadow-md shadow-gray-600 checked:bg-purple-500 bg-slate-100"
       />
-      <Icon
-        icon="ph:check-circle"
-        class="cursor-pointer"
-        color="#8b5cf6"
-        width="25"
-        v-else
-        @click="$emit('complete-editing', index)"
-      />
-      <Icon
-        icon="ph:trash"
-        class="cursor-pointer"
-        color="#F87171"
-        :key="todo.id"
-        @click="$emit('delete-todo', index)"
-        width="25"
-      />
+      <div class="">
+        <input
+          type="text"
+          v-if="todo.isEditing"
+          @input="$emit('update-todo', $event.target.value, index)"
+          class="rounded-lg px-3 object-fill text-gray-600"
+          :value="todo.todo"
+        />
+        <span
+          v-else
+          class="text-gray-700 pe-9"
+          :class="{ 'line-through': todo.isComplete }"
+          >{{ todo.todo }}</span
+        >
+      </div>
+      <div
+        class="flex gap-3 absolute right-3 group-hover:opacity-100 opacity-0 transition duration-500"
+      >
+        <Icon
+          icon="ph:pen-fill"
+          @click="$emit('edit-todo', index)"
+          class="cursor-pointer"
+          :key="todo.id"
+          v-if="!todo.isEditing"
+          color="#34D399"
+          width="25"
+        />
+        <Icon
+          icon="ph:check-circle"
+          class="cursor-pointer"
+          color="#8b5cf6"
+          width="25"
+          v-else
+          @click="$emit('complete-editing', index)"
+        />
+        <Icon
+          icon="ph:trash"
+          class="cursor-pointer"
+          color="#F87171"
+          :key="todo.id"
+          @click="$emit('delete-todo', index)"
+          width="25"
+        />
+      </div>
     </div>
   </li>
 </template>
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,53 +1,63 @@
 <script setup>
-import TodoCreator from "../components/TodoCreator.vue";
-import { computed, ref, watch } from "vue";
-import { uid } from "uid";
-import TodoItem from "../components/TodoItem.vue";
-import { Icon } from "@iconify/vue";
-let todoList = ref([]);
-watch(
-  todoList,
-  () => {
-    localStorage.setItem("todoList", JSON.stringify(todoList.value));
-  },
-  {
-    deep: true,
-  }
-);
-const todosCompleted = computed(() => {
-  return todoList.value.every((todo)=>todo.isComplete)
-})
-console.log(todosCompleted)
-const fetchTodoList = () => {
-  const todoValue = JSON.parse(localStorage.getItem("todoList"));
-  if (todoValue) {
-    todoList.value = todoValue;
-  }
-};
-fetchTodoList();
-const addTodo = (todo) => {
-  todoList.value.push({
-    id: uid(),
-    todo,
-    isComplete: false,
-    isEditing: false,
+  import TodoCreator from "../components/TodoCreator.vue";
+  import {
+    computed,
+    ref,
+    watch
+  } from "vue";
+  import {
+    uid
+  } from "uid";
+  import TodoItem from "../components/TodoItem.vue";
+  import {
+    Icon
+  } from "@iconify/vue";
+  import 
+    Draggable
+   from "vuedraggable";
+  let todoList = ref([]);
+  watch(
+    todoList,
+    () => {
+      localStorage.setItem("todoList", JSON.stringify(todoList.value));
+    }, {
+      deep: true,
+    }
+  );
+  const todosCompleted = computed(() => {
+    return todoList.value.every((todo) => todo.isComplete)
   })
-};
-const deleteTodo = (index) => {
-  todoList.value.splice(index, 1)
-};
-const editTodo = (index) => {
-  todoList.value[index].isEditing = true
-};
-const onToggleComplete = (index) => {
-  todoList.value[index].isComplete = !todoList.value[index].isComplete
-};
-const onCompleteEditing = (index) => {
-  todoList.value[index].isEditing = false
-};
-const onUpdateTodo = (value, index) => {
-  todoList.value[index].todo = value
-};
+  console.log(todosCompleted)
+  const fetchTodoList = () => {
+    const todoValue = JSON.parse(localStorage.getItem("todoList"));
+    if (todoValue) {
+      todoList.value = todoValue;
+    }
+  };
+  fetchTodoList();
+  const addTodo = (todo) => {
+    todoList.value.push({
+      id: uid(),
+      todo,
+      isComplete: false,
+      isEditing: false,
+    })
+  };
+  const deleteTodo = (index) => {
+    todoList.value.splice(index, 1)
+  };
+  const editTodo = (index) => {
+    todoList.value[index].isEditing = true
+  };
+  const onToggleComplete = (index) => {
+    todoList.value[index].isComplete = !todoList.value[index].isComplete
+  };
+  const onCompleteEditing = (index) => {
+    todoList.value[index].isEditing = false
+  };
+  const onUpdateTodo = (value, index) => {
+    todoList.value[index].todo = value
+  };
 </script>
 
 <template>
@@ -56,13 +66,18 @@ const onUpdateTodo = (value, index) => {
   </h2>
   <TodoCreator @form-submitted="addTodo" />
   <p class="flex gap-4 justify-center" v-if="todoList.length==0">
-    <Icon icon="noto-v1:sad-but-relieved-face" width="20" ></Icon>
-    <p class="text-sm text-gray-600">You have no todo's to complete ! Add one!</p>
+    <Icon icon="noto-v1:sad-but-relieved-face" width="20"></Icon>
+    <p class="text-sm text-gray-600 ">You have no todo's to complete ! Add one!</p>
   </p>
 
-  <ul class="text-center flex items-center flex-col">
+ <Draggable  class="text-center flex items-center flex-col"  
+  v-model="todoList" 
+  tag="ul"
+  drag-class="drag"
+  ghost-class="ghost"
+  >
+  <template #item="{ element: todo,index }">
     <TodoItem
-      v-for="(todo, index) in todoList"
       @edit-todo="editTodo"
       @delete-todo="deleteTodo"
       @toggle-complete="onToggleComplete"
@@ -72,9 +87,20 @@ const onUpdateTodo = (value, index) => {
       :todo="todo"
       :index="index"
     ></TodoItem>
-  </ul>
-  <p class="flex gap-4 justify-center m-4 transition-all duration-1000 "  v-show="todosCompleted && todoList.length>0">
-    <Icon icon="noto-v1:party-popper" width="20" ></Icon>
+  </template>
+</Draggable>
+
+  <p class="flex gap-4 justify-center m-4 transition-all duration-1000  " v-show="todosCompleted && todoList.length>0">
+    <Icon icon="noto-v1:party-popper" width="20"></Icon>
     <p class="text-sm text-gray-600">You have complete all your tasks!</p>
   </p>
 </template>
+<style>
+.drag .drag-div {
+  transform: rotate(10deg) !important;
+}
+.ghost input , .ghost div {
+  visibility: hidden;
+  transition: 1s;
+}
+</style>
