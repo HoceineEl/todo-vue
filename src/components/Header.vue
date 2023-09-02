27c2ca993ae908149420a56dbd1a6cbe813dbc18
Hoceine <hoceineelidrissi@gmail.com>
2023-09-02 15:40:42 +0100

    edit the header styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,8 @@
 import { RouterLink } from "vue-router";
 </script>
 <template>
-  <header>
-    <nav
-      class="flex justify-between items-center bg-gradient-to-r from-purple-950 to-purple-400"
-    >
+  <header class="bg-gradient-to-r from-purple-950 to-purple-400">
+    <nav class="flex container justify-between items-center mx-auto">
       <img class="w-32" src="../assets/imgs/todo-logo.png" />
       <ul class="flex gap-10 font-semibold text-indigo-50 mr-8">
         <RouterLink to="/">Home</RouterLink>
